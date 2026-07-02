2bc97190eba8392e378c6189a7e9bd697f11b955
Christopher <117981104+amadratitus@users.noreply.github.com>
2026-07-02 03:13:30 +0200

    Change license from MIT to GPL-2.0-only

--- hdc3022.c
+++ hdc3022.c
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: MIT License
+// SPDX-License-Identifier: GPL-2.0-only
 /*
+ * HDC3022 temperature and humidity sensor driver (character device) 
  * Hardware:
  *   - Adafruit HDC3022 breakout
  *   - Adafruit MCP2221A USB-to-I2C bridge
